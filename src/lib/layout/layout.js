d60b7738ca4a32b8a2a667fdbd9f85907dd61cc7
kuozhi <kuozhi@kuozhideMBP-2.lan>
2019-10-28 19:24:07 +0800

    编译一下啊

--- src/lib/layout/layout.js
+++ src/lib/layout/layout.js
@@ -91,7 +91,7 @@ class Layout extends Component {
     if (item.nodes && item.nodes.length) {
       this.options.output += `<ul>`
       for (let list of item.nodes) {
-        this.options.output += `<li class='cd-group-item'><a id=${list.id} class='cd-group-item_link' title=${list.name}  data-grade=${list.grade} href=${list.link ? list.link : 'javascript:;'} data-href=${list.link}><span class='item-txt'>${list.name}</span></a>`;
+        this.options.output += `<li class='cd-group-item'><a id=${list.id} class='cd-group-item_link' target='${targetVal}' title=${list.name}  data-grade=${list.grade} href=${list.link ? list.link : 'javascript:;'} data-href=${list.link}><span class='item-txt'>${list.name}</span></a>`;
         this.creatElement(list)
       }
       this.options.output += `</ul>`
